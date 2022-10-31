ba742702cec43f02d6fbdbcdf68b0c988a7ee8f3
JaredJSun <jiajun_sun@tamu.edu>
2022-10-30 22:36:53 -0500

    add 2 movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,20 +7,20 @@
 #   Character.create(name: "Luke", movie: movies.first)
 # Seed the RottenPotatoes DB with some movies.
 more_movies = [
-    {:title => 'My Neighbor Totoro', :rating => 'G',
-      :release_date => '16-Apr-1988'},
-    {:title => 'Green Book', :rating => 'PG-13',
-      :release_date => '16-Nov-2018'},
-    {:title => 'Parasite', :rating => 'R',
-      :release_date => '30-May-2019'},
-    {:title => 'Nomadland', :rating => 'R',
-      :release_date => '19-Feb-2021'},
-    {:title => 'CODA', :rating => 'PG-13',
-      :release_date => '13-Aug-2021'}
     {:title => 'Let the Bullets Fly', :rating => 'N/A',
-      :release_date => '16-Dec-2010'}
+        :release_date => '16-Dec-2010'},
     {:title => 'WALL-E', :rating => 'G',
-      :release_date => '27-Jun-2008'}
+        :release_date => '27-Jun-2008'},
+    # {:title => 'My Neighbor Totoro', :rating => 'G',
+    #   :release_date => '16-Apr-1988'},
+    # {:title => 'Green Book', :rating => 'PG-13',
+    #   :release_date => '16-Nov-2018'},
+    # {:title => 'Parasite', :rating => 'R',
+    #   :release_date => '30-May-2019'},
+    # {:title => 'Nomadland', :rating => 'R',
+    #   :release_date => '19-Feb-2021'},
+    # {:title => 'CODA', :rating => 'PG-13',
+    #   :release_date => '13-Aug-2021'}
   ]
   
   more_movies.each do |movie|
